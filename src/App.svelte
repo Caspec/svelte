aa336448f4402625e835103aa9581cb27370c50b
Caspec <quii2000@yahoo.dk>
2025-06-25 22:45:28 +0200

    -

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let name = "Abe";
+	let age = 30;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name} {age}!</h1>
+<button on:click={incrementAge}>
+	Increment Age
+</button>
