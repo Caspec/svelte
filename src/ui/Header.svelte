310654ba70fd1a0fda93529ca7b8c2e46d3a6a01
Caspec <quii2000@yahoo.dk>
2025-06-30 15:56:59 +0200

    feat: add user store and ts

---
+++ src/ui/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<style>
+header {
+		background-color: #282c34;
+		color: white;
+		padding: 20px;
+		text-align: center;
+	}
+</style>
+
+<header>
+	<h1>Welcome to Meetup!</h1>
+</header>
---
+++ src/ui/MainContent.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import UserData from "../meetups/UserData.svelte";
+</script>
+
+<style>
+main {
+    padding: 20px;
+    background-color: #f0f0f0;
+    min-height: calc(100vh - 200px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
+
+<main>
+    <UserData />
+</main>
